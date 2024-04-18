fdc9bb280cc532bce637a68d37953df1ad278e80
STEVEBAPU <165133766+STEVEBAPU@users.noreply.github.com>
2024-04-18 20:10:05 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["playing : mc.nexpixel.net","playing:mc.nexpixel.net"];
 
 
 let currentIndex = 0;
